e274995a735d8cabdd5fd5e669bfaa692d365f01
KolDamer <kena3727@gmail.com>
2024-08-23 19:21:12 +0000

    all essentials

---
+++ work-area/src/App.jsx
@@ -0,0 +1,21 @@
+
+import Header from './Header'
+import Body from './Body'
+import Footer from './footer'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+    
+      <Header/>
+      <Body/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ work-area/src/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Body() {
+  return (
+    <div >
+        <div  className=''>
+            <h>CHASE JECT</h>
+        </div>
+    </div>
+  )
+}
+
+export default Body
